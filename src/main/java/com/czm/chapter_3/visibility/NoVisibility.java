10895b0fb22476ffd99a43cca6d4c66265f256b2
687396077879 <czm_19951003@163.com>
2022-03-15 00:45:56 +0800

    可见性demo

---
+++ src/main/java/com/czm/chapter_3/visibility/NoVisibility.java
@@ -0,0 +1,27 @@
+package com.czm.chapter_3.visibility;
+
+/**
+ * @author jeromecui
+ * @description
+ * @date 2022/3/15 0:42
+ */
+public class NoVisibility {
+    private static boolean ready;
+    private static int number;
+
+    private static class ReaderThread extends Thread {
+        @Override
+        public void run() {
+            while (!ready) {
+                Thread.yield();
+            }
+            System.out.println(number);
+        }
+    }
+
+    public static void main(String[] args) {
+        new ReaderThread().start();
+        number = 42;
+        ready = true;
+    }
+}
